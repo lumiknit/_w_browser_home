a410dc1fa498b0f09574db4f69c58cd00190971e
lumiknit <aasr4r4@gmail.com>
2024-07-10 11:32:14 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Component, createSignal, For, Match, onMount, Show, Switch } from 'solid-js'
-
+import {
+  Component,
+  createSignal,
+  For,
+  Match,
+  onMount,
+  Show,
+  Switch,
+} from "solid-js";
 
 export type Shortcut = {
   name: string;
   href: string;
   icon: string;
-}
+};
 
 export type Data = {
   timeAlign: "left" | "center" | "right";
@@ -19,10 +26,10 @@ const defaultData: Data = {
   timeAlign: "center",
   shortcuts: [
     {
-      "name": "Google",
-      "href": "https://www.google.com/",
-      "icon": "https://www.google.com/favicon.ico",
-    }
+      name: "Google",
+      href: "https://www.google.com/",
+      icon: "",
+    },
   ],
   shortcutMargin: 30,
   shortcutRows: 3,
@@ -30,35 +37,42 @@ const defaultData: Data = {
 };
 
 const shortcutsToString = (shortcuts: Shortcut[]): string => {
-  return shortcuts.map(s => `${s.name} ; ${s.href} ; ${s.icon}`).join("\n\n");
+  return shortcuts
+    .map((s) => `${s.name}\n${s.href}\n${s.icon}`.trim())
+    .join("\n\n")
+    .trim();
 };
 
 const stringToShortcuts = (s: string): Shortcut[] => {
-  return s.split("\n").map(s => s.trim()).filter(s => s).map(l => {
-    const [name, href, icon] = l.split(";").map(s => s.trim());
-    return {
-      name: name || "Link",
-      href: href || "#",
-      icon: icon || "",
-    };
-  });
+  return s
+    .split("\n\n")
+    .map((s) => s.trim())
+    .filter((s) => s)
+    .map((l) => {
+      const [name, href, icon] = l.split("\n").map((s) => s.trim());
+      return {
+        name: name || "Link",
+        href: href || "#",
+        icon: icon || "",
+      };
+    });
 };
 
 const dataShortcutsToString = (data: Data): string => {
   const s = shortcutsToString(data.shortcuts);
   const f = shortcutsToString(data.foldedShortcuts);
-  return s + "\n\n---\n\n" + f;
+  return s + "\n\n-----\n\n" + f;
 };
 
 const stringToDataShortcuts = (s: string): Partial<Data> => {
-  const [shortcuts, foldedShortcuts] = s.split("---");
+  const [shortcuts, foldedShortcuts] = s.split("-----");
   return {
     shortcuts: stringToShortcuts(shortcuts),
     foldedShortcuts: stringToShortcuts(foldedShortcuts),
   };
 };
 
-const storageName = "__home_shortcuts_data"
+const storageName = "__home_shortcuts_data";
 
 const loadData = (): Data => {
   // Load from local storage
@@ -66,6 +80,8 @@ const loadData = (): Data => {
   try {
     return { ...defaultData, ...(data ? JSON.parse(data) : {}) };
   } catch (e) {
+    // Reset if error
+    saveData(defaultData);
     return { ...defaultData };
   }
 };
@@ -73,7 +89,6 @@ const loadData = (): Data => {
 const saveData = (data: Data) => {
   localStorage.setItem(storageName, JSON.stringify(data));
 };
-
 
 /**
  * Return hh:mm in 24-hour format
@@ -85,24 +100,34 @@ const get24HourTime = (): string => {
   return `${hours}:${minutes}`;
 };
 
-const Shortcuts: Component<{ shortcuts: Shortcut[], w: number }> = props => {
-  return <div class="shortcuts">
-    <For each={props.shortcuts}>
-      {
-        s => <div class="s-w" style={{
-          "flex": `0 0 ${props.w}%`,
-        }}><a class="shortcut" href={s.href}
-        >
-            <img src={
-              s.icon || "https://www.google.com/s2/favicons?sz=128&sz=64&sz=32&domain=" + new URL(s.href).hostname
-            } alt={s.name} />
-            <div class="label">{s.name}</div>
-          </a>
-        </div>
-      }
-    </For>
-  </div>;
-}
+const Shortcuts: Component<{ shortcuts: Shortcut[]; w: number }> = (props) => {
+  return (
+    <div class="shortcuts">
+      <For each={props.shortcuts}>
+        {(s) => (
+          <div
+            class="s-w"
+            style={{
+              flex: `0 0 ${props.w}%`,
+            }}
+          >
+            <a class="shortcut" href={s.href}>
+              <img
+                src={
+                  s.icon ||
+                  "https://www.google.com/s2/favicons?sz=128&sz=64&sz=32&domain=" +
+                    new URL(s.href).hostname
+                }
+                alt={s.name}
+              />
+              <div class="label">{s.name}</div>
+            </a>
+          </div>
+        )}
+      </For>
+    </div>
+  );
+};
 
 const App: Component = () => {
   const [data, setData] = createSignal<Data>(loadData());
@@ -119,7 +144,6 @@ const App: Component = () => {
     setInterval(() => {
       setTime(get24HourTime());
     }, 20000);
-    updateData(d => d);
   });
 
   return (
@@ -130,7 +154,15 @@ const App: Component = () => {
           <div>
             <label>
               Time alignment:
-              <select value={data().timeAlign} onChange={e => updateData(d => ({ ...d, timeAlign: e.currentTarget.value as any }))}>
+              <select
+                value={data().timeAlign}
+                onChange={(e) =>
+                  updateData((d) => ({
+                    ...d,
+                    timeAlign: e.currentTarget.value as any,
+                  }))
+                }
+              >
                 <option value="left">Left</option>
                 <option value="center">Center</option>
                 <option value="right">Right</option>
@@ -141,51 +173,103 @@ const App: Component = () => {
           <div>
             <label>
               Shortcut margin:
-              <input type="number" value={data().shortcutMargin} onChange={e => updateData(d => ({ ...d, shortcutMargin: parseInt(e.currentTarget.value) }))} />
+              <input
+                type="number"
+                value={data().shortcutMargin}
+                onChange={(e) =>
+                  updateData((d) => ({
+                    ...d,
+                    shortcutMargin: parseInt(e.currentTarget.value),
+                  }))
+                }
+              />
               svh
             </label>
           </div>
-          <input type="range" min={0} max={100} value={data().shortcutMargin} onChange={e => updateData(d => ({ ...d, shortcutMargin: parseInt(e.currentTarget.value) }))} />
+          <input
+            type="range"
+            min={0}
+            max={100}
+            value={data().shortcutMargin}
+            onChange={(e) =>
+              updateData((d) => ({
+                ...d,
+                shortcutMargin: parseInt(e.currentTarget.value),
+              }))
+            }
+          />
           <div>
             <label>
               Shortcut rows:
-              <input type="number" value={data().shortcutRows} onChange={e => updateData(d => ({ ...d, shortcutRows: parseInt(e.currentTarget.value) }))} />
+              <input
+                type="number"
+                value={data().shortcutRows}
+                onChange={(e) =>
+                  updateData((d) => ({
+                    ...d,
+                    shortcutRows: parseInt(e.currentTarget.value),
+                  }))
+                }
+              />
             </label>
           </div>
           <h1> Shortcut List </h1>
+          <ul>
+            <li> Each item should be separated by a double newline </li>
+            <li> Each item should have 3 lines: Name, URL, Icon URL </li>
+          </ul>
           <textarea
-            onChange={
-              e => updateData(d => ({ ...d, ...stringToDataShortcuts(e.currentTarget.value) }))
+            onChange={(e) =>
+              updateData((d) => ({
+                ...d,
+                ...stringToDataShortcuts(e.currentTarget.value),
+              }))
             }
             rows={10}
-          >{dataShortcutsToString(data())}</textarea>
+          >
+            {dataShortcutsToString(data())}
+          </textarea>
         </Match>
         <Match when>
-          <div class="clock" style={{
-            "text-align": data().timeAlign,
-          }}>
-            <span class="clock-text">
-              {time()}
-            </span>
+          <div
+            class="clock"
+            style={{
+              "text-align": data().timeAlign,
+            }}
+          >
+            <span class="clock-text">{time()}</span>
           </div>
           <Show when={data().shortcutMargin}>
-            <div class="m" style={{
-              "height": data().shortcutMargin + "svh",
-            }} />
+            <div
+              class="m"
+              style={{
+                height: data().shortcutMargin + "svh",
+              }}
+            />
           </Show>
-          <Shortcuts shortcuts={data().shortcuts} w={100 / data().shortcutRows} />
+          <Shortcuts
+            shortcuts={data().shortcuts}
+            w={100 / data().shortcutRows}
+          />
           <Show when={!folding()}>
-            <Shortcuts shortcuts={data().foldedShortcuts} w={100 / data().shortcutRows} />
+            <Shortcuts
+              shortcuts={data().foldedShortcuts}
+              w={100 / data().shortcutRows}
+            />
           </Show>
           <Show when={data().foldedShortcuts.length > 0}>
-            <button class="btn-fold" onClick={() => setFolding(v => !v)}>{folding() ? "v" : "^"}</button>
+            <button class="btn-fold" onClick={() => setFolding((v) => !v)}>
+              {folding() ? "∨" : "∧"}
+            </button>
           </Show>
         </Match>
       </Switch>
 
-      <button class="btn-edit" onClick={() => setEditing(v => !v)}>{editing() ? "Done" : "Edit"}</button>
+      <button class="btn-edit" onClick={() => setEditing((v) => !v)}>
+        {editing() ? "Done" : "Edit"}
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   plugins: [solid(), viteSingleFile()],
   build: {
-		target: ["esnext", "firefox110", "chrome110", "safari15"],
-	},
-})
+    target: ["esnext", "firefox110", "chrome110", "safari15"],
+  },
+});
